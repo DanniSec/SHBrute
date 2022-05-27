7f946b9bda9a3f05d31dc9ae9ef721b7ac010f24
SoudruhDanny <54038831+SoudruhDanny@users.noreply.github.com>
2022-05-27 08:26:09 +0000

    added new flags for input box names

--- SHBrute.py
+++ SHBrute.py
@@ -8,21 +8,28 @@ from rich.console import Console
 console = Console()
 
 parser = argparse.ArgumentParser(description='SHBrute - Simple Http *Form* Bruteforcer')
-parser.add_argument('-t', '--target', metavar='url', help='Whole url to the HTTP login form ex. "http://web.com/login"')
 
+# REQUIRED FLAGS
+parser.add_argument('-t', '--target', metavar='url', help='Whole url to the HTTP login form ex. "http://web.com/login"')
 parser.add_argument('-u', '--user', metavar='name', help='single username (For wordlist use -U)')
 parser.add_argument('-U', '--users', metavar='wordlist', help='wordlist option for username')
 
 parser.add_argument('-p', '--password', metavar='password', help='single password (For wordlist use -P')
 parser.add_argument('-P', '--passwords', metavar='wordlist', help='wordlist option for password')
 
+parser.add_argument('-uf', metavar='str', help='login form name')
+parser.add_argument('-pf', metavar='str', help='password form name')
 
-parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
 parser.add_argument('-f', '--form', metavar='num', help='http form number (if there are multiple forms)', type=int)
 parser.add_argument('-w', '--wait', metavar='seconds', help='wait time between requests', type=float)
-parser.add_argument('-F̈́', '--fail', metavar='str', help='response with failed login', type=str)
-args = parser.parse_args()
+parser.add_argument('-F', '--fail', metavar='str', help='response with failed login', type=str)
 
+# Optional flags
+parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
+
+
+
+args = parser.parse_args()
 print('[bold]SHBrute[/]')
 
 
@@ -45,8 +52,8 @@ def load_configuration():
 
 def try_configuration(url, user, password):
     br.select_form(nr=args.form)
-    br.form['username'] = user
-    br.form['password'] = password
+    br.form[args.uf] = user
+    br.form[args.pf] = password
     response = br.submit()
     
     if args.fail in str(response.read()):
